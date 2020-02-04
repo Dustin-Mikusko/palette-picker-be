36e64dc4e07db34854d19173769104d6f512f3a6
Dustin Mikusko <dustinjm713@yahoo.com>
2020-02-04 07:17:50 -0700

    Remove second migration due to error, and link the tables correctly

--- db/migrations/20200203183040_initial.js
+++ db/migrations/20200203183040_initial.js
@@ -19,16 +19,22 @@ exports.up = function(knex) {
     })
     .createTable('projects', table => {
       table.increments('id').primary();
+      table.integer('user_id').unsigned();
       table.foreign('user_id')
         .references('users.id');
+      table.integer('color_1_id').unsigned();
       table.foreign('color_1_id')
         .references('colors.id');
+      table.integer('color_2_id').unsigned();
       table.foreign('color_2_id')
         .references('colors.id');
+      table.integer('color_3_id').unsigned();  
       table.foreign('color_3_id')
         .references('colors.id');
+      table.integer('color_4_id').unsigned();   
       table.foreign('color_4_id')
         .references('colors.id');
+      table.integer('color_5_id').unsigned();
       table.foreign('color_5_id')
         .references('colors.id');
 
