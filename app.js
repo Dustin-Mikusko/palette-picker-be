06ff7d0fb7f1e64a68c16cfc184b09e7989a61e4
Alan Turing <austeyd@gmail.com>
2020-02-04 16:04:59 -0700

    Install and configure babel

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-
+const app = express();
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
-
-const app = express();
 
 app.locals.title = 'Palette Picker';
 app.use(cors());
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './app';
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
