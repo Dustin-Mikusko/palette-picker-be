7612eede3af7df06731ee3558d2d598474ea2574
Alan Turing <austeyd@gmail.com>
2020-02-04 15:38:26 -0700

    Add seed property to knexfile

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-const app = express();
 
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
 
+const app = express();
+
 app.locals.title = 'Palette Picker';
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('oh hello')
+})
 
 export default app;
--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 import app from './app';
 
 app.set('port', process.env.PORT || 3001);
-app.locals.title = 'Palette Picker';
-
-app.get('/', (req, res) => {
-  res.send('oh hello')
-})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
