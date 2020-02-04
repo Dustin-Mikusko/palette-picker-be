04019cd2c1e178226a58fe4681d8df8a15420a6e
Dustin Mikusko <dustinjm713@yahoo.com>
2020-02-04 08:22:20 -0700

    Augment scraped colors to fit data table and seed colors table

--- seeds/colors.js
+++ seeds/colors.js
@@ -1,10 +1,15 @@
 const colors = require('../colors');
 
+const hexColors = colors.map(color => ({
+  hex_code: color
+}));
+
+
 exports.seed = async function(knex) {
   try {
     await knex('colors').del();
 
-    return knex('colors').insert(colors);
+    return knex('colors').insert(hexColors);
   } catch(error) {
     console.log(`Error seeding data: ${error}`)
   }
