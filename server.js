bab2cab32c5aca638faa13de4fbf71b17d2b6c64
Alan Turing <austeyd@gmail.com>
2020-02-04 15:19:26 -0700

    Migrate configuration code from server.js to the new app.js file

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+import app from './app';
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Palette Picker';
@@ -11,8 +6,6 @@ app.locals.title = 'Palette Picker';
 app.get('/', (req, res) => {
   res.send('oh hello')
 })
- 
-
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
