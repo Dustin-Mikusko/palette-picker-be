0690b9e36f110c4a3d38a8c74f84af8abfb1e92e
Alan Turing <austeyd@gmail.com>
2020-02-04 21:09:01 -0700

    Add projects data to seed files

--- db/seeds/dev/colors.js
+++ db/seeds/dev/colors.js
@@ -1,5 +1,6 @@
 const colors = require('../../../colors');
 const users = require('../../../users');
+const projects = require('../../../projects');
 
 const hexColors = colors.map(color => ({
   hex_code: color
@@ -11,11 +12,24 @@ const theUsers = users.map(user => ({
   name: user.name
 }));
 
+const theProjects = projects.map(user => ({
+  user_id: project.user_id,
+  color_1_id: project.color_1_id,
+  color_2_id: project.color_2_id,
+  color_3_id: project.color_3_id,
+  color_4_id: project.color_4_id,
+  color_5_id: project.color_5_id,
+}));
+
 exports.seed = async function(knex) {
   try {
+    await knex('colors').del();
     await knex.raw('ALTER SEQUENCE colors_id_seq RESTART WITH 1');
     await knex('users').del();
     await knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
+    await knex('projects').del();
+    await knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
+    await knex('projects').insert(theUsers);
     await knex('users').insert(theUsers);
 
     return knex('colors').insert(hexColors);
