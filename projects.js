bbf252cf09c2011be4a8ff109dee88645caba096
Alan Turing <austeyd@gmail.com>
2020-02-05 15:43:58 -0700

    Revise seed data for projects

--- projects.js
+++ projects.js
@@ -1,20 +1,47 @@
-const users = [
+const projects = [
   {
-    user_id: 1,
-    color_1_id: "#bbbbbb",
-    color_1_id: "#690303",
-    color_1_id: "#000000",
-    color_1_id: "#add097",
-    color_1_id: "#928400"'
+    title: 'kitchen',
+    palettes: [
+      {
+        title: "Floor",
+        color_1_id: "#bbbbbb",
+        color_2_id: "#690303",
+        color_3_id: "#000000",
+        color_4_id: "#add097",
+        color_5_id: "#928400"
+      },
+      {
+        title: "ceiling",
+        color_1_id: "#bb23bb",
+        color_2_id: "#690311",
+        color_3_id: "#ab0000",
+        color_4_id: "#adhd97",
+        color_5_id: "#928411"
+      }
+    ]
   },
   {
-    user_id: 2,
-    color_1_id: "#bbb1bb",
-    color_1_id: "#690303",
-    color_1_id: "#034200",
-    color_1_id: "#a2d097",
-    color_1_id: "#911400"'
-  },
+    title: 'bathroom',
+    palettes: [
+      {
+        title: "sink",
+        color_1_id: "#bbaabb",
+        color_2_id: "#691103",
+        color_3_id: "#00bf00",
+        color_4_id: "#ccd097",
+        color_5_id: "#ff8400"
+      },
+      {
+        title: "counter",
+        color_1_id: "#bb211b",
+        color_2_id: "#220311",
+        color_3_id: "#ab3300",
+        color_4_id: "#55hd97",
+        color_5_id: "#929911"
+      }
+    ]
+  }
 ];
 
-module.exports = users;
+
+module.exports = projects;
