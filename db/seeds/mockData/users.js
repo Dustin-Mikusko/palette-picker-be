307882e1a0d94110ad74faca690360c06b935959
Alan Turing <austeyd@gmail.com>
2020-02-04 18:14:20 -0700

    Finish users mockData file

--- db/seeds/mockData/users.js
+++ db/seeds/mockData/users.js
@@ -1,6 +1,8 @@
 const users = [
-  {user_id: 1, email: 'Dustin@example.com' , password: 'Dirty D' , name: 'Dustin'}
-  { name: 'Susan', password: 'Susan', id: 2 }
+    {email: 'Dustin@example.com' , password: 'Dirty D' , name: 'Dustin'},
+    {email: 'Austen@example.com' , password: 'AD' , name: 'Austen'},
+    {email: 'Joe@example.com' , password: 'JoJo' , name: 'Joey'}
+
   ];
 
 module.exports = users;
