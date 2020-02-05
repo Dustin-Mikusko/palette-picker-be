c345e3e57ac7e8dd961bad913f015abb8915805d
Alan Turing <austeyd@gmail.com>
2020-02-04 18:15:07 -0700

    Add endpoint for getting a specific user

--- app.js
+++ app.js
@@ -11,6 +11,27 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('oh hello')
-})
+});
+
+app.get('/api/v1/users/:id:', async(request, response) => {
+  const { id } = request.params;
+  console.log(request.params);
+
+  try {
+    const user = await database('students').where('id', id);
+
+    if (user.length) {
+      response.status(200).json({user})
+    } else {
+      response.status(404).json({
+        error: `Could not find a user with id: ${request.params.id}`
+      });
+    }
+  } catch (error) {
+    response.status(500).json({error: 'internal server error' })
+  }
+});
+
+
 
 export default app;
