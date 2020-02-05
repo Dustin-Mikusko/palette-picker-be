8ec3cf6224ed9fafdf4bd1465bb901d32f1eb037
Alan Turing <austeyd@gmail.com>
2020-02-04 18:14:52 -0700

    Add happy test for getting specific user

--- app.test.js
+++ app.test.js
@@ -14,4 +14,19 @@ describe('Server', () => {
       expect(res.status).toBe(200)
     });
   });
+
+  describe('GET /api/v1/users/:id', () => {
+    it('should return a happy status code of 200 and a single user object', async () => {
+      const expectedUser = await database('users').first().select();
+      console.log(expectedUser);
+      const { id } = expectedUser;
+
+      const response = await request(app).get(`/api/v1/students/${id}`);
+      console.log(response.body);
+      const result = response.body[0];
+
+      expect(response.status).toBe(200);
+      expect(result).toEqual(expectedUser);
+    });
+  });
 });
