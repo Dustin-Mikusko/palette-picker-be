5fb77c7e8da6e086b59a212a61340157de07b624
Alan Turing <austeyd@gmail.com>
2020-02-05 15:44:27 -0700

    Remove obsolete users file

--- users.js
+++
@@ -1,6 +0,0 @@
-const users = [
-    {email: 'Dustin@example.com' , password: 'password' , name: 'Dustin D'},
-    {email: 'Austen@example.com' , password: 'password' , name: 'Austen D'}
-  ];
-
-module.exports = users;
