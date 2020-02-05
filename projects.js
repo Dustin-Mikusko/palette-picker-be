803bf8f8fd17b7ffa4bac22eb1852523677f2d4e
Alan Turing <austeyd@gmail.com>
2020-02-04 21:06:18 -0700

    Add default projects data

---
+++ projects.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    user_id: 1,
+    color_1_id: "#bbbbbb",
+    color_1_id: "#690303",
+    color_1_id: "#000000",
+    color_1_id: "#add097",
+    color_1_id: "#928400"'
+  },
+  {
+    user_id: 2,
+    color_1_id: "#bbb1bb",
+    color_1_id: "#690303",
+    color_1_id: "#034200",
+    color_1_id: "#a2d097",
+    color_1_id: "#911400"'
+  },
+];
+
+module.exports = users;
