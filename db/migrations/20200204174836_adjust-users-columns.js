3a41cb4f574ebf9af062f649677f4b84fb616d57
Alan Turing <austeyd@gmail.com>
2020-02-04 18:03:10 -0700

    Revise migration to remove created and updated at columns

--- db/migrations/20200204174836_adjust-users-columns.js
+++ db/migrations/20200204174836_adjust-users-columns.js
@@ -1,9 +1,11 @@
 
 exports.up = function(knex) {
-  return knex.schema.tables('users', table => {
+  return knex.schema.table('users', table => {
     table.dropColumn('palette_1_id');
     table.dropColumn('palette_2_id');
     table.dropColumn('palette_3_id');
+    table.dropColumn('created_at');
+    table.dropColumn('updated_at'');
     table.string('password');
   })
 };
