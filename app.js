dbbcb2eed58e7826d5cb8ec2e790eb5ffbbde21e
Alan Turing <austeyd@gmail.com>
2020-02-05 21:16:11 -0700

    Add implementation code for GET project

--- app.js
+++ app.js
@@ -15,5 +15,22 @@ app.get('/', (req, res) => {
   res.send('oh hello')
 });
 
+app.get('/api/v1/projects/:id', async(request, response) => {
+  const { id } = request.params;
+  try {
+    const project = await database('projects').where('id', id);
+
+    if (project.length) {
+      response.status(200).json(project)
+    } else {
+      response.status(404).json({
+        error: `Could not find a project with id: ${request.params.id}`
+      });
+    }
+  } catch (error) {
+    response.status(500).json({error: 'internal server error' })
+  }
+});
+
 
 export default app;
