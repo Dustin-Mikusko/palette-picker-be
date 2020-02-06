5deea0d3da8eca616ce89dcf014b74cc840731a8
Alan Turing <austeyd@gmail.com>
2020-02-05 21:15:57 -0700

    Add happy path test for GET project

--- app.test.js
+++ app.test.js
@@ -19,4 +19,33 @@ describe('Server', () => {
     });
   });
 
+  // //GET one project
+  describe('GET /api/v1/projects/:id', () => {
+    it('should return a happy status code of 200 and a single project object', async () => {
+      const expectedProject = await database('projects').first();
+      delete expectedProject.created_at;
+      delete expectedProject.updated_at;
+      const { id } = expectedProject;
+
+      const response = await request(app).get(`/api/v1/projects/${id}`);
+      const result = response.body[0];
+      delete result.created_at;
+      delete result.updated_at;
+      
+      expect(response.status).toBe(200);
+      expect(result).toEqual(expectedProject);
+    });
+  });
+
+  // //GET all project
+  // api/v1/projects
+  // //Delete a project
+  // 'api/v1/projects/:id'
+  //   but first
+  // Deletes all attached palettes   api/v1/projects/id:/palettes'
+  // //POST a project
+  // 'api/v1/projects'
+  // //PUT title edit
+
+
 });
