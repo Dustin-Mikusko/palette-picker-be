65d02d2cde58f82365dbf00e94a68579ccb022dd
Dustin Mikusko <dustinjm713@yahoo.com>
2020-02-08 11:02:59 -0700

    Update knexfile for Heroku

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
-const database = require('knex')(configuration)
-import '@babel/polyfill';
+
 
 const app = express();
 app.locals.title = 'Palette Picker';
 app.use(cors());
 app.use(express.json());
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+import '@babel/polyfill';
 
 app.get('/', (req, res) => {
   res.send('oh hello')
